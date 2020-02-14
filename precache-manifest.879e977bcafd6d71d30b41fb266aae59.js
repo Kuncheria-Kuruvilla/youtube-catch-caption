828a6108fe75a98c22c0b4f31b240a72b05721fa
Kuncheria-Kuruvilla <kuncheria94@gmail.com>
2020-02-15 01:51:31 +0530

    Updates

---
+++ precache-manifest.879e977bcafd6d71d30b41fb266aae59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a14e6141912b84e5017c512f6079b11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5f96a67cd48b5c76dea",
+    "url": "/static/css/main.a9facb40.chunk.css"
+  },
+  {
+    "revision": "2e61428f4a87c3785552",
+    "url": "/static/js/2.65ea67b2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.65ea67b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f96a67cd48b5c76dea",
+    "url": "/static/js/main.f9e560db.chunk.js"
+  },
+  {
+    "revision": "cd22a78f70fc16bf4d84",
+    "url": "/static/js/runtime-main.20b11e29.js"
+  }
+]);
